73f27ed12f4b85ce6a40d59ae205b496589df714
ADONIX\adoni <adonix56@gmail.com>
2023-10-10 02:30:11 -0700

    Added Door Interaction Component

---
+++ Source/CH02Doors/CH02Doors.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CH02Doors.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH02Doors, "CH02Doors" );
