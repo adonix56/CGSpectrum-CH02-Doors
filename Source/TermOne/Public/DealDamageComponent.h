8ae16487457254330009d170fcfef47fa4bfb642
ADONIX\adoni <adonix56@gmail.com>
2023-11-07 02:04:08 -0800

    Added StarterContent to project and gitignore. Created DealdamageComponent and added a damaging fire trigger.

---
+++ Source/TermOne/Public/DealDamageComponent.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DealDamageComponent.generated.h"
+
+class UCapsuleComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class TERMONE_API UDealDamageComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UDealDamageComponent();
+
+	UFUNCTION()
+	void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& Swee);
+
+	UFUNCTION()
+	void OnOverlapEnd(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float BaseDamage = 20.0f;
+
+	UPROPERTY(EditAnywhere, NoClear)
+	UCapsuleComponent* TriggerCapsule;
+		
+};
